c9ca3934a68564cf76a68b09456fb77bc9bc0f19
Marcin Zdunek <marcinzdunek100@gmail.com>
2024-07-29 22:49:54 +0200

    Add input helper function

--- helpers.py
+++ helpers.py
@@ -6,5 +6,17 @@ def readArgs():
     channel_id = str(sys.argv[3])
     spotify_client_id = str(sys.argv[4])
     spotify_client_secret = str(sys.argv[5])
-    return yt_client_id, yt_client_secret, channel_id, spotify_client_id, spotify_client_secret
+    spotify_playlist = str(sys.argv[6])
+    return yt_client_id, yt_client_secret, channel_id, spotify_client_id, spotify_client_secret, spotify_playlist
     
+def get_integer_input(prompt: str):
+    while True:
+        try:
+            user_input = input(prompt)
+            value = int(user_input)
+            if 0 <= value <= 10:
+                return value
+            else: 
+                print("Value must be from 0 to 10")
+        except ValueError:
+            print("Invalid input, number must be from 0 to 10")
