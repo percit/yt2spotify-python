c61bdd606f38133f29ac6681ba744aa10bc474e3
Marcin Zdunek <marcinzdunek100@gmail.com>
2024-07-29 19:10:17 +0200

    Add api for spotify

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import helpers
-import yt_auth
+import yt_api
+import spotify_api
 
 def main():
-    yt_client_id, yt_client_secret, channel_id spotify_client_id, spotify_client_secret = helpers.readArgs()
+    yt_client_id, yt_client_secret, channel_id, spotify_client_id, spotify_client_secret = helpers.readArgs()
 
     # client = yt_auth.do_authorize(yt_client_id, yt_client_secret) 
     # this may not work now, because it takes time to change redirect uri in google console
